7e1f781929bf3277625961fd0226e82393ee4fd0
tumacz <124058702+tumacz@users.noreply.github.com>
2025-09-07 15:27:32 +0200

    Update CachedChunk.cs

--- Assets/Scripts/PlanetTerrain/Runtime/TerrainStreaming/Cache/CachedChunk.cs
+++ Assets/Scripts/PlanetTerrain/Runtime/TerrainStreaming/Cache/CachedChunk.cs
@@ -1,28 +1,3 @@
-//using System.Collections.Generic;
-//using UnityEngine;
-//using GlobeRTS.PlanetTerrain.Runtime.Geometry;
-
-//namespace GlobeRTS.PlanetTerrain.Runtime.TerrainStreaming.Cache
-//{
-//    /// <summary>Description of a single cached tile.</summary>
-//    public sealed class CachedChunk
-//    {
-//        public TileKey key;
-
-//        public GameObject go;
-//        public Mesh mesh;
-//        public TileDeformer_GPU deformer;
-
-//        public long sizeBytes;
-//        public float lastAccessTime;
-//        public bool isActive;
-
-//        public int lastVisibleStamp;
-
-//        // LRU
-//        public LinkedListNode<TileKey> lruNode;
-//    }
-//}
 using System.Collections.Generic;
 using UnityEngine;
 using GlobeRTS.PlanetTerrain.Runtime.Geometry;
